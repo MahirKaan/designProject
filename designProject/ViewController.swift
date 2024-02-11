70fb4364be030c52b7ccef42e749c53c23113cb4
Mahir Kaan Küçükgençay <149965910+MahirKaan@users.noreply.github.com>
2024-02-11 16:37:41 +0300

    Initial Commit

---
+++ designProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  designProject
+//
+//  Created by Mahir Kaan Küçükgençay on 11.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
